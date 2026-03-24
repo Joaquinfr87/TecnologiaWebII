372bc77cb8974eccb7506e0d5aedc12e5ec9361b
DogmaG04 <progamok@gmail.com>
2026-03-24 08:47:12 -0400

    feat(model): agregar modelo Rol

---
+++ backend/app/Models/Rol.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model
+{
+    protected $table = 'Rol';
+    protected $primaryKey = 'Id_Rol';
+    public $timestamps = false;
+    protected $fillable = ['Nombre'];
+}
