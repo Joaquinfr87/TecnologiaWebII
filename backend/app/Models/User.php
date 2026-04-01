7fbf7a5537e61f6bf47bcc7fdf2629066e2292b9
DogmaG04 <progamok@gmail.com>
2026-04-01 15:28:24 -0400

    refactor(user): configurar modelo para soportar llaves UUID

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, HasUuids;
 
     protected $table = 'Usuario';
     protected $primaryKey = 'Id_Usuario';
+    public $incrementing = false;
+    protected $keyType = 'string';
     public $timestamps = false;
 
     protected $fillable = [
